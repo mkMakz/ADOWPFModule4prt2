8f1b9024e618bb80c57cfb83a3575c5c55d64a2e
mkMakz <33726337+mkMakz@users.noreply.github.com>
2018-07-09 14:47:40 +0600

    Add project files.

---
+++ ADOWPFModule4prt2/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADOWPFModule4prt2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetData_Click(object sender, RoutedEventArgs e)
+        {
+            GroupFrame.Source = new Uri("Pages/ListGroup.xaml", UriKind.RelativeOrAbsolute);
+        }
+
+        private void GetData2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GetData3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void GetData4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ADOWPFModule4prt2/Pages/ListGroup.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADOWPFModule4prt2.Pages
+{
+    /// <summary>
+    /// Interaction logic for ListGroup.xaml
+    /// </summary>
+    public partial class ListGroup : Page
+    {
+    private static SqlConnection con = new SqlConnection();
+        public ListGroup()
+        {
+            InitializeComponent();
+
+            con.ConnectionString = @"Server=DESKTOP-9QBKPFG\LOCALDB;Database=CRCMS_new;Trusted_Connection=True;";
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Group";
+
+            DataSet ds = new DataSet();
+            DataTable tbl = ds.Tables.Add("Group");
+
+            SqlDataAdapter da =
+                new SqlDataAdapter(cmd);
+
+            con.Open();
+
+            da.Fill(ds);
+
+            con.Close();
+
+           Group.ItemsSource
+
+
+        }
+    }
+}
